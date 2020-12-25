f83fa41d42c4df66ebfa7ccc0c1b37d5cd15e30a
Emile Nijssen <emile@emilenijssen.nl>
2020-12-25 13:13:49 +0100

    Initial Commit

---
+++ Homey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Homey
+//
+//  Created by Emile Nijssen on 25/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Homey/HomeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeyApp.swift
+//  Homey
+//
+//  Created by Emile Nijssen on 25/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct HomeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
